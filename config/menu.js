9fcfc5a6c5024a62018a68b52f9d069bd62799af
bmagic <baptiste@bmagic.fr>
2019-03-03 20:40:15 +0100

    Fix menu.js

--- config/menu.js
+++ config/menu.js
@@ -1,19 +1,8 @@
 const menu = [
   { type: 'config', id: 'news' },
-  { type: 'config', id: 'live' },
   { type: 'config', id: 'tickets' },
   { type: 'config', id: 'tournaments' },
-  { type: 'config',
-    id: 'info',
-    children: [
-      { type: 'config', id: 'tournaments' },
-      { type: 'config', id: 'live' },
-      { type: 'config', id: 'live' },
-      { type: 'page', title: 'Une page', id: 6, link: '/test' },
-      { type: 'config', id: 'live' },
-      { type: 'config', id: 'live' },
-      { type: 'page', title: 'Une page encore', id: 7, link: '/urlquelonveut' }
-    ] },
+  { type: 'config', id: 'info' },
   { type: 'config', id: 'partners' }
 ]
 
