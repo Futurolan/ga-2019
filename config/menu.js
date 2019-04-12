16e030cb3c3e9857add31ee76203b96f3c2d614f
mupfpilou <41705573+mupfpilou@users.noreply.github.com>
2019-04-12 14:34:29 +0200

    Update menu.js

--- config/menu.js
+++ config/menu.js
@@ -12,6 +12,7 @@ const menu = [
       { type: 'config', id: 'family' },
       { type: 'page', title: 'Cosplay', id: 2673, link: '/cosplay' },
       { type: 'page', title: 'Les Offs', id: 2727, link: '/offs' },
+      { type: 'page', title: 'Concours', id: 3175, link: '/concours' },
       { type: 'page', title: 'Grand Poitiers', id: 1246, link: '/grand-poitiers' }
     ] },
   { type: 'config', id: 'partners' }
